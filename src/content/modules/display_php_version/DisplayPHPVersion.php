4afae3ecb4be1eacb6997bdda6663e40f077b3ba
Ulrich Schmidt <admin@deruli.de>
2017-08-09 21:24:24 +0200

    refakturiert, versionsnummer korrigiert

--- src/content/modules/display_php_version/DisplayPHPVersion.php
+++ src/content/modules/display_php_version/DisplayPHPVersion.php
@@ -1,7 +1,12 @@
 <?php
-class DisplayPHPVersion extends Controller {
-	private $moduleName = "display_php_version";
-	public function accordionLayout() {
-		return Template::executeModuleTemplate ( $this->moduleName, "phpversion.php" );
-	}
+
+class DisplayPHPVersion extends Controller
+{
+
+    private $moduleName = "display_php_version";
+
+    public function accordionLayout()
+    {
+        return Template::executeModuleTemplate($this->moduleName, "phpversion.php");
+    }
 }
